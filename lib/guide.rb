4c5278d795258f222dc259ceba6e0e613d9495eb
Ankur Zilpelwar <ankur.zilpelwar@gmail.com>
2014-05-16 19:28:51 +0400

    Create body for defs- file_usable?, file_exists? create_file in restaurant class.

--- lib/guide.rb
+++ lib/guide.rb
@@ -5,17 +5,16 @@ class Guide
 			#locate the restraunt text file at path
 			Restaurant.filepath = path 
 
-			if Restaurant.file_exists?
+			if Restaurant.file_usable?
 				puts "Found Restaurant file."
+			#or create a new file
 			elsif Restaurant.create_file
 				puts "Created restaurant file."
+			#exit if create fails	
 			else
 				puts "Exiting \n \n"
 				exit!		#exit the application -ruby command
 			end
-					
-			#or create a new file
-			#exit if create fails
 	end	
 
 	def launch!
--- lib/restaurant.rb
+++ lib/restaurant.rb
@@ -3,15 +3,34 @@ class Restaurant
 	@@filepath=nil
 
 	def self.filepath=(path=nil)	#setter method for filepath variable
+		@@filepath =File.join(APP_ROOT,path)
 
 	end
 
 	def self.file_exists?
 		#class should know if the restaurant file file_exists
+		if @@filepath && File.exists?(@@filepath)
+			return true
+		else
+			return false
+		end
 	end
+
+	def self.file_usable?
+
+		return false unless @@filepath
+		return false unless File.exists?(@@filepath)
+		return false unless File.readable?(@@filepath)
+		return false unless File.writable?(@@filepath)
+		return true
+
+	end
+
 
 	def self.create_file
 		#create a restaurant file
+		File.open(@@filepath,'w') unless file_exists?
+		return file_usable?
 	end
 
 	def self.saved_restaurants
